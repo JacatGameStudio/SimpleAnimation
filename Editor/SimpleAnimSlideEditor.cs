e91289709ef72f42db884fc5ca0ad3fee6e406d5
Quynv <quynv@volio.vn>
2022-10-18 17:34:31 +0700

    simple anim slide can manual edit

--- Editor/SimpleAnimSlideEditor.cs
+++ Editor/SimpleAnimSlideEditor.cs
@@ -31,14 +31,12 @@ namespace Omnilatent.SimpleAnimation
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-            bool hasChange = false;
             GUILayout.Space(15);
 
             GUILayout.BeginHorizontal();
             EditorGUILayout.PropertyField(posStart, GUILayout.MaxWidth(450));
             if (GUILayout.Button("Get Pos", GUILayout.MaxWidth(80)))
             {
-                hasChange = true;
                 if (rect != null)
                     slideAnim.PosStart = rect.anchoredPosition;
                 else
@@ -50,18 +48,15 @@ namespace Omnilatent.SimpleAnimation
             EditorGUILayout.PropertyField(posEnd, GUILayout.MaxWidth(450));
             if (GUILayout.Button("Get Pos", GUILayout.MaxWidth(80)))
             {
-                hasChange = true;
                 if (rect != null)
                     slideAnim.PosEnd = rect.anchoredPosition;
                 else
                     slideAnim.PosEnd = slideAnim.transform.localPosition;
             }
             GUILayout.EndHorizontal();
-            if (hasChange)
-            {
-                serializedObject.ApplyModifiedProperties();
-                EditorUtility.SetDirty(slideAnim);
-            }
+
+            serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(slideAnim);
         }
     }
 }
